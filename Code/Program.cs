f4106704c4e0eaddb015cbf6181032d2f3be71ba
Natasha <a1@192.168.1.3>
2022-09-11 11:50:11 +0300

    Final changes

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символа.
+
+string[] FirstArray = {"dfghdkbfv", "2fsgbgb", "ffb", "sbsdfb", "44", "2fsgbgb", "ffb", "sbsdfb", "44"};
+string[] SecondArray = new string[FirstArray.Length];
+void FillArray(string[] FirstArray, string[] SecondArray)
+{
+    int count = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+    if(FirstArray[i].Length <= 3)
+        {
+        SecondArray[count] = FirstArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(FirstArray, SecondArray);
+PrintArray(SecondArray);
